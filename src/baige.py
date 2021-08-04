ed757bcb11e5dc19bee58ebd9e258f28114b284e
sidruns30 <siddhantsolanki@ucsb.edu>
2021-08-03 20:43:28 -0700

    added extra cell to the face centered arrays

--- src/baige.py
+++ src/baige.py
@@ -40,23 +40,23 @@ class face_centered():
     def __init__(self, nx, ny, nz, ngh, xi, xf, yi, yf, zi, zf, **kwargs):
         
         # Coordinates (do not contain ghost zones)
-        self.x = np.linspace(xi + (xf - xi)/nx, xf - (xf - xi)/nx, nx - 1)
-        self.y = np.linspace(yi + (yf - yi)/ny, yf - (yf - yi)/ny, ny - 1)
-        self.z = np.linspace(zi + (zf - zi)/nz, zf - (zf - zi)/nz, nz - 1)
+        self.x = np.linspace(xi + (xf - xi)/nx, xf - (xf - xi)/nx, nx + 1)
+        self.y = np.linspace(yi + (yf - yi)/ny, yf - (yf - yi)/ny, ny + 1)
+        self.z = np.linspace(zi + (zf - zi)/nz, zf - (zf - zi)/nz, nz + 1)
         
         # Primitives
-        self.prim_rho = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1))
-        self.prim_velx = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1))
-        self.prim_vely = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1))
-        self.prim_velz = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1))
-        self.prim_press = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1))
+        self.prim_rho = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1))
+        self.prim_velx = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1))
+        self.prim_vely = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1))
+        self.prim_velz = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1))
+        self.prim_press = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1))
         
         # Fluxes for conservatives
-        self.flux_rho = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1, 3))
-        self.flux_momx = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1, 3))
-        self.flux_momy = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1, 3))
-        self.flux_momz = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1, 3))
-        self.flux_en = np.zeros((nx + 2*ngh - 1, ny + 2*ngh - 1, nz + 2*ngh - 1, 3))
+        self.flux_rho = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1, 3))
+        self.flux_momx = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1, 3))
+        self.flux_momy = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1, 3))
+        self.flux_momz = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1, 3))
+        self.flux_en = np.zeros((nx + 2*ngh + 1, ny + 2*ngh + 1, nz + 2*ngh + 1, 3))
 
         # Gas variable(s)
         if 'gas_gamma' not in kwargs: self.gas_gamma = 5/3
